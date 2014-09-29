05aec80543ac63ef1ed345088bf713bc8677513c
Sanjay <sjchakrav@gmail.com>
2014-09-29 15:29:55 -0400

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,6 +1,6 @@
 /* 
-Create a program that prints "Fizz" fizz for multiples of 3, "buzz" for multiples #of 5, 
-"FizzBuzz" for multiples of 3 AND 5, and the actual raw number itself for #everything else 
+Create a program that prints "Fizz" fizz for multiples of 3, "buzz" for multiples of 5, 
+"FizzBuzz" for multiples of 3 AND 5, and the actual raw number itself for everything else 
 within the range of 1-100 
 */
 
