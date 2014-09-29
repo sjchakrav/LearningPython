8050d407ee589456dad55fc8176fc56aba967b1e
Sanjay <sjchakrav@gmail.com>
2014-09-29 15:56:03 -0400

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,8 +1,8 @@
-/* 
+'''
 Create a program that prints "Fizz" fizz for multiples of 3, "buzz" for multiples of 5, 
 "FizzBuzz" for multiples of 3 AND 5, and the actual raw number itself for everything else 
 within the range of 1-100 
-*/
+'''
 
 for i in xrange(1,101):
 		x = ""
